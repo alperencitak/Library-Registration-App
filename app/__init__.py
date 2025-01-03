bf5e0cd755d1c42f6fc72ab573a4fde539b6a317
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-03 23:39:42 +0300

    Correction book_category class

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from config import Config
 from flask_migrate import Migrate
 from .api.auth_api import auth_bp
 from .api.category_api import category_bp
+from .api.book_api import book_bp
 
 
 def create_app():
@@ -12,10 +13,11 @@ def create_app():
 
     db.init_app(app)
 
-    from .models import base, user, book, bookcategory, category, loan, author
+    from .models import base, user, book, book_category, category, loan, author
 
     app.register_blueprint(auth_bp, url_prefix="/auth")
     app.register_blueprint(category_bp, url_prefix="/category")
+    app.register_blueprint(book_bp, url_prefix="/book")
 
     Migrate(app, db)
 
--- app/models/book_category.py
+++ app/models/book_category.py
@@ -6,6 +6,3 @@ class BookCategory(db.Model):
 
     book_id = db.Column(db.Integer, db.ForeignKey('books.id'), primary_key=True)
     category_id = db.Column(db.Integer, db.ForeignKey('categories.id'), primary_key=True)
-
-    book = db.relationship("Book", back_populates="book_categories")
-    category = db.relationship("Category", back_populates="book_categories")
