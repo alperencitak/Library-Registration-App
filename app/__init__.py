24bff36793573a503b89aaaf9a6a712e9ec172a8
Alperen Çıtak <146676581+alperencitak@users.noreply.github.com>
2025-01-04 19:28:48 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,8 @@ from .api.auth_api import auth_bp
 from .api.category_api import category_bp
 from .api.book_api import book_bp
 from .api.author_api import author_bp
+from .api.loan_api import loan_bp
+from .api.user_api import user_bp
 
 
 def create_app():
@@ -20,6 +22,8 @@ def create_app():
     app.register_blueprint(category_bp, url_prefix="/category")
     app.register_blueprint(book_bp, url_prefix="/book")
     app.register_blueprint(author_bp, url_prefix="/author")
+    app.register_blueprint(loan_bp, url_prefix="/loan")
+    app.register_blueprint(user_bp, url_prefix="/user")
 
     Migrate(app, db)
 
